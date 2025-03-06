32cd8be698e52dc47993bb6debcf4fddcb72df7c
raj <rajasekarbooramoorthi@gmail.com>
2025-03-06 14:21:48 +0530

    Dependency

--- app/src/main/java/com/cart/ecart/application/ECartApplication.kt
+++ app/src/main/java/com/cart/ecart/application/ECartApplication.kt
@@ -1,7 +1,45 @@
 package com.cart.ecart.application
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
+import android.util.Log
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class ECartApplication : Application()
+class ECartApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+                Log.d("ActivityCallback", "${activity.localClassName} Created")
+            }
+
+            override fun onActivityStarted(activity: Activity) {
+                Log.d("ActivityCallback", "${activity.localClassName} Started")
+            }
+
+            override fun onActivityResumed(activity: Activity) {
+                Log.d("ActivityCallback", "${activity.localClassName} Resumed")
+            }
+
+            override fun onActivityPaused(activity: Activity) {
+                Log.d("ActivityCallback", "${activity.localClassName} Paused")
+            }
+
+            override fun onActivityStopped(activity: Activity) {
+                Log.d("ActivityCallback", "${activity.localClassName} Stopped")
+            }
+
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+                Log.d("ActivityCallback", "${activity.localClassName} SaveInstanceState")
+            }
+
+            override fun onActivityDestroyed(activity: Activity) {
+                Log.d("ActivityCallback", "${activity.localClassName} Destroyed")
+            }
+        })
+    }
+}
