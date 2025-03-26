07d8678e91ab6b6dafe28cac588763098574e87d
raj <rajasekarbooramoorthi@gmail.com>
2025-03-26 22:53:51 +0530

    Dependency

--- app/src/main/java/com/cart/ecart/application/ECartApplication.kt
+++ app/src/main/java/com/cart/ecart/application/ECartApplication.kt
@@ -11,35 +11,5 @@ class ECartApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
-        registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
-            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
-                Log.d("ActivityCallback", "${activity.localClassName} Created")
-            }
-
-            override fun onActivityStarted(activity: Activity) {
-                Log.d("ActivityCallback", "${activity.localClassName} Started")
-            }
-
-            override fun onActivityResumed(activity: Activity) {
-                Log.d("ActivityCallback", "${activity.localClassName} Resumed")
-            }
-
-            override fun onActivityPaused(activity: Activity) {
-                Log.d("ActivityCallback", "${activity.localClassName} Paused")
-            }
-
-            override fun onActivityStopped(activity: Activity) {
-                Log.d("ActivityCallback", "${activity.localClassName} Stopped")
-            }
-
-            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
-                Log.d("ActivityCallback", "${activity.localClassName} SaveInstanceState")
-            }
-
-            override fun onActivityDestroyed(activity: Activity) {
-                Log.d("ActivityCallback", "${activity.localClassName} Destroyed")
-            }
-        })
     }
 }
